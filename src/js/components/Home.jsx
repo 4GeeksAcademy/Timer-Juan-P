bfb07a0052a92f520b36809c076d4118697552ac
Pipinto <juanpinto07@gmail.com>
2025-02-12 16:21:30 +0000

    First Commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+  const [counter, setCounter] = useState(0);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+  useEffect(() => {
+    const intervalId = setInterval(() => setCounter(counter + 1), 1000);
+    return () => clearInterval(intervalId);
+  }, [counter]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  // Cálculos de días, horas, minutos y segundos
+  const days = Math.floor(counter / 86400); // 1 día = 86400 segundos
+  const hours = Math.floor((counter % 86400) / 3600); // 1 hora = 3600 segundos
+  const minutes = Math.floor((counter % 3600) / 60); // 1 minuto = 60 segundos
+  const seconds = counter % 60; // Segundos restantes
+
+  return (
+    <section>
+      <div className="container text-center">
+        {/* Lista que contiene los textos y los valores */}
+        <ul className="list-unstyled position-absolute top-50 start-50 translate-middle d-flex justify-content-between w-50">
+          
+          {/* Primer grupo de elementos con los textos encima */}
+          <li className="d-flex flex-column align-items-center">
+            <span className="h3 bg-dark text-white p-3 rounded">Days</span>
+            <span className="h1 bg-light-subtle border border-dark p-3 rounded">{days}</span>
+          </li>
+          <li className="d-flex flex-column align-items-center">
+            <span className="h3 bg-dark text-white p-3 rounded">Hours</span>
+            <span className="h1 bg-light-subtle border border-dark p-3 rounded">{hours}</span>
+          </li>
+          <li className="d-flex flex-column align-items-center">
+            <span className="h3 bg-dark text-white p-3 rounded">Mins</span>
+            <span className="h1 bg-light-subtle border border-dark p-3 rounded">{minutes}</span>
+          </li>
+          <li className="d-flex flex-column align-items-center">
+            <span className="h3 bg-dark text-white p-3 rounded">Secs</span>
+            <span className="h1 bg-light-subtle border border-dark p-3 rounded">{seconds}</span>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
 
 export default Home;
+
+
+
